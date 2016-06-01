a191a735c238efd910321d7af4d34126f6dde65d
root <root@localhost.localdomain>
2016-06-01 09:16:18 -0400

    first

--- _/components/js/script.js
+++ _/components/js/script.js
@@ -1,1 +1,14 @@
-//
+$(document).ready(function(){
+    $("div").click(function() {
+        var rightArrowParents = [];
+        $(this).parents().addBack().not('html').each(function() {
+          var entry = this.tagName.toLowerCase();
+          if (this.className) {
+            entry += "." + this.className.replace(/ /g, '.');
+          }
+          rightArrowParents.push(entry);
+        });
+        console.log (rightArrowParents.join(" "));
+        return false;
+    });
+});
